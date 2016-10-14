040d4654edc8858260ef207475bd2ceb7b9abd97
Stewart McCoy <stewart.mccoy@causes.com>
2016-10-14 18:10:04 -0500

    first commit

---
+++ button.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+import Button from 'react-native-button';
+
+export default class ExampleComponent extends Component {
+  constructor(props, context) {
+    super(props, context);
+  }
+  _handlePress() {
+    console.log('Pressed!');
+  }
+  render() {
+    return (
+      <Button
+        style={{
+          fontSize: 20,
+          backgroundColor: 'green',
+          color: 'white',
+          padding: 10,
+        }}
+        styleDisabled={{color: 'red'}}
+        onPress={() => this._handlePress()}>
+        Press Me!
+      </Button>
+    );
+  }
+};
---
+++ index.ios.js
@@ -0,0 +1,71 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+import Button from './button';
+
+class ConstitutionQuiz extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Hello world!</Text>
+        <Button />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ffffff',
+  },
+  // welcome: {
+  //   fontSize: 20,
+  //   textAlign: 'center',
+  //   margin: 10,
+  // },
+  // instructions: {
+  //   textAlign: 'center',
+  //   color: '#333333',
+  //   marginBottom: 5,
+  // },
+});
+
+AppRegistry.registerComponent('ConstitutionQuiz', () => ConstitutionQuiz);
+
+// import React, { Component } from 'react';
+// import {
+//   AppRegistry,
+//   StyleSheet,
+//   Text,
+//   View
+// } from 'react-native';
+//
+// export default class ConstitutionQuiz extends Component {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Text style={styles.welcome}>
+//           Welcome to React Native!
+//         </Text>
+//         <Text style={styles.instructions}>
+//           To get started, edit index.ios.js
+//         </Text>
+//         <Text style={styles.instructions}>
+//           Press Cmd+R to reload,{'\n'}
+//           Cmd+D or shake for dev menu
+//         </Text>
+//       </View>
+//     );
+//   }
+// }
+//
+//
+// AppRegistry.registerComponent('ConstitutionQuiz', () => ConstitutionQuiz);
